b54493296def880dc271d7d52e7a25da7024103a
matrixhaolin <linhao@matrix.space>
2018-10-29 11:59:35 +0800

    modify port number

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -16,8 +16,8 @@ var (
 
 const (
 	GasLimitBoundDivisor uint64 = 1024    // The bound divisor of the gas limit, used in update calculations.
-	MinGasLimit          uint64 = 1507900000    // Minimum the gas limit may ever be.
-	GenesisGasLimit      uint64 = 1507900000 // Gas limit of the Genesis block.
+	MinGasLimit          uint64 = 630000000 // Minimum the gas limit may ever be.
+	GenesisGasLimit      uint64 = 630000000	// Gas limit of the Genesis block.
 
 	MaximumExtraDataSize  uint64 = 32    // Maximum size extra data may be after Genesis.
 	ExpByteGas            uint64 = 10    // Times ceil(log256(exponent)) for the EXP instruction.
