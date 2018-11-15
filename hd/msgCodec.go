39efd63bc6d3a7f1aeb4250abd188ea423b64a5a
matrixhaolin <linhao@matrix.space>
2018-11-15 19:51:01 +0800

    changes to msgcodec.go

--- hd/msgCodec.go
+++ hd/msgCodec.go
@@ -70,9 +70,6 @@ func (*blkConsensusReqCodec) DecodeFn(data []byte, from common.Address) (interfa
 	if err != nil {
 		return nil, errors.Errorf("json.Unmarshal failed: %s", err)
 	}
-	if msg.Header == nil {
-		return nil, errors.Errorf("'header' of the msg is nil")
-	}
 	msg.From.Set(from)
 	return msg, nil
 }
@@ -121,9 +118,6 @@ func (*miningReqCodec) DecodeFn(data []byte, from common.Address) (interface{}, 
 	if err != nil {
 		return nil, errors.Errorf("json.Unmarshal failed: %s", err)
 	}
-	if msg.Header == nil {
-		return nil, errors.Errorf("'header' of the msg is nil")
-	}
 	msg.From.Set(from)
 	return msg, nil
 }
@@ -147,9 +141,6 @@ func (*miningRspCodec) DecodeFn(data []byte, from common.Address) (interface{}, 
 	err := json.Unmarshal([]byte(data), msg)
 	if err != nil {
 		return nil, errors.Errorf("json.Unmarshal failed: %s", err)
-	}
-	if msg.Difficulty == nil {
-		return nil, errors.Errorf("'Difficulty' of the msg is nil")
 	}
 	msg.From.Set(from)
 	return msg, nil
@@ -189,9 +180,6 @@ func (*broadcastMiningRspCodec) DecodeFn(data []byte, from common.Address) (inte
 	if err != nil {
 		return nil, errors.Errorf("json.Unmarshal failed: %s", err)
 	}
-	if msg.Header == nil {
-		return nil, errors.Errorf("'Header' of the msg is nil")
-	}
 
 	sendMsg := &mc.HD_BroadcastMiningRspMsg{
 		From: from,
@@ -229,9 +217,6 @@ func (*newBlockInsertCodec) DecodeFn(data []byte, from common.Address) (interfac
 	err := json.Unmarshal([]byte(data), msg)
 	if err != nil {
 		return nil, errors.Errorf("json.Unmarshal failed: %s", err)
-	}
-	if msg.Header == nil {
-		return nil, errors.Errorf("'Header' of the msg is nil")
 	}
 	msg.From.Set(from)
 	return msg, nil
@@ -379,9 +364,6 @@ func (*lrReqCodec) DecodeFn(data []byte, from common.Address) (interface{}, erro
 	err := json.Unmarshal([]byte(data), msg)
 	if err != nil {
 		return nil, errors.Errorf("json.Unmarshal failed: %s", err)
-	}
-	if msg.InquiryReq == nil {
-		return nil, errors.Errorf("'InquiryReq' of the msg is nil")
 	}
 	msg.InquiryReq.From.Set(from)
 	return msg, nil
