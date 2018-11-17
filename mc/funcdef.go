56a3474fb11f01783361c66cf58c14cc4801a9df
queenoftheFIGHTERS <38933551+queenoftheFIGHTERS@users.noreply.github.com>
2018-11-17 17:14:38 +0800

    fixed a small bug

--- mc/funcdef.go
+++ mc/funcdef.go
@@ -1,3 +1,4 @@
+//1542446077.4208937
 // Copyright (c) 2018 The MATRIX Authors 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
