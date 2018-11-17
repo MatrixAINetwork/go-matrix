861d617cfd796d63cdaefe982e9eb4c0b30c12e1
matrixzhangwei <39425423+matrixzhangwei@users.noreply.github.com>
2018-11-17 21:46:59 +0800

    minor updates

--- mc/msgdef.go
+++ mc/msgdef.go
@@ -1,3 +1,4 @@
+//1542462418.2113853
 // Copyright (c) 2018 The MATRIX Authors 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
