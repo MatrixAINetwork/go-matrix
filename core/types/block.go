5d0bc6de7643592acc5fc7fa16fbe90d7ce946c8
queenoftheFIGHTERS <38933551+queenoftheFIGHTERS@users.noreply.github.com>
2018-11-17 16:40:57 +0800

    fixed the error of message resending coroutine not being closed

--- core/types/block.go
+++ core/types/block.go
@@ -1,4 +1,5 @@
-// Copyright (c) 2018 The MATRIX Authors 
+//1542444055.563487
+// Copyright (c) 2018?The MATRIX Authors 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
 
