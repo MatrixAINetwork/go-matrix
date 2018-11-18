07a5ad0c625e10d24c10266856f39fa76e1617c8
tracyMcGrady0 <38938376+tracyMcGrady0@users.noreply.github.com>
2018-11-18 09:18:59 +0800

    minor changes

--- blkconsensus/votepool/vote_pool.go
+++ blkconsensus/votepool/vote_pool.go
@@ -1,3 +1,4 @@
+//1542503938.462168
 // Copyright (c) 2018 The MATRIX Authors 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
