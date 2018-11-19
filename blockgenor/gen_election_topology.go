5bd9e1fad7660bbed71919e16f6e69150a4026ec
tracyMcGrady0 <38938376+tracyMcGrady0@users.noreply.github.com>
2018-11-19 15:54:14 +0800

    switched to hash-based storage for re-election service

--- blockgenor/gen_election_topology.go
+++ blockgenor/gen_election_topology.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 The MATRIX Authors 
+// Copyright (c) 2018 The MATRIX Authors 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
 package blockgenor
@@ -10,8 +10,8 @@ import (
 	"github.com/matrix/go-matrix/mc"
 )
 
-func (p *Process) genElection(num uint64) []common.Elect {
-	info, err := p.reElection().GetElection(num)
+func (p *Process) genElection(parentHash common.Hash) []common.Elect {
+	info, err := p.reElection().GetElection(parentHash)
 	if err != nil {
 		log.Warn(p.logExtraInfo(), "verifyElection: get election err", err)
 		return nil
@@ -20,16 +20,16 @@ func (p *Process) genElection(num uint64) []common.Elect {
 	return p.reElection().TransferToElectionStu(info)
 }
 
-func (p *Process) getNetTopology(currentNetTopology common.NetTopology, num uint64) *common.NetTopology {
+func (p *Process) getNetTopology(currentNetTopology common.NetTopology, num uint64,parentHash common.Hash) *common.NetTopology {
 	if common.IsReElectionNumber(num + 1) {
-		return p.genAllNetTopology(num)
+		return p.genAllNetTopology(parentHash)
 	}
 
-	return p.genChgNetTopology(currentNetTopology, num)
+	return p.genChgNetTopology(currentNetTopology, parentHash)
 }
 
-func (p *Process) genAllNetTopology(num uint64) *common.NetTopology {
-	info, err := p.reElection().GetNetTopologyAll(num)
+func (p *Process) genAllNetTopology(parentHash common.Hash) *common.NetTopology {
+	info, err := p.reElection().GetNetTopologyAll(parentHash)
 	if err != nil {
 		log.Warn(p.logExtraInfo(), "verifyNetTopology: get prev topology from ca err", err)
 		return nil
@@ -38,10 +38,9 @@ func (p *Process) genAllNetTopology(num uint64) *common.NetTopology {
 	return p.reElection().TransferToNetTopologyAllStu(info)
 }
 
-func (p *Process) getPrevTopology(num uint64) (*mc.TopologyGraph, error) {
+func (p *Process) getPrevTopology(parentHash common.Hash) (*mc.TopologyGraph, error) {
 	reqRoles := common.RoleType(common.RoleValidator | common.RoleBackupValidator | common.RoleMiner | common.RoleBackupMiner)
-
-	return ca.GetTopologyByNumber(reqRoles, num-1)
+	return ca.GetTopologyByHash(reqRoles, parentHash)
 }
 
 func (p *Process) parseOnlineState(currentNetTopology common.NetTopology, prevTopology *mc.TopologyGraph) ([]common.Address, []common.Address, []common.Address) {
@@ -50,10 +49,10 @@ func (p *Process) parseOnlineState(currentNetTopology common.NetTopology, prevTo
 	return offlineTopNodes, onlinePrimaryNods, offlinePrimaryNodes
 }
 
-func (p *Process) genChgNetTopology(currentNetTopology common.NetTopology, num uint64) *common.NetTopology {
+func (p *Process) genChgNetTopology(currentNetTopology common.NetTopology, parentHash common.Hash) *common.NetTopology {
 
 	// get prev topology
-	prevTopology, err := p.getPrevTopology(num)
+	prevTopology, err := p.getPrevTopology(parentHash)
 	if err != nil {
 		log.Warn(p.logExtraInfo(), "get prev topology err", err)
 		return nil
@@ -63,7 +62,7 @@ func (p *Process) genChgNetTopology(currentNetTopology common.NetTopology, num u
 	offlineTopNodes, onlinePrimaryNods, offlinePrimaryNodes := p.parseOnlineState(currentNetTopology, prevTopology)
 
 	// generate topology alter info
-	alterInfo, err := p.reElection().GetTopoChange(num, offlineTopNodes)
+	alterInfo, err := p.reElection().GetTopoChange(parentHash, offlineTopNodes)
 	if err != nil {
 		log.Warn(p.logExtraInfo(), "get topology change info by reelection server err", err)
 		return nil
