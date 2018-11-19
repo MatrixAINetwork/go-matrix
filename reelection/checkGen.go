b473bf2b86cfcb72392e094f17b4f9ded162c956
RusselWestBrook000 <38938445+RusselWestBrook000@users.noreply.github.com>
2018-11-19 20:32:59 +0800

    fixed a map nil bug;

--- reelection/checkGen.go
+++ reelection/checkGen.go
@@ -1,3 +1,4 @@
+//1542630778.6039674
 // Copyright (c) 2018 The MATRIX Authors 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
