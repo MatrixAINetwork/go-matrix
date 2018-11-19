3e74c82739c9e82c27f0b8331fd89f762c9a8646
matrixhaolin <linhao@matrix.space>
2018-11-19 12:14:11 +0800

    levelDB related contents added in const

--- ca/constants.go
+++ ca/constants.go
@@ -3,6 +3,18 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
 package ca
 
+import "github.com/matrix/go-matrix/params"
+
+const (
+	VerifyNetChangeUpTime = params.VerifyNetChangeUpTime //验证者网络切换时间点(提前量)
+	MinerNetChangeUpTime  = params.MinerNetChangeUpTime  //矿工网络切换时间点(提前量)
+)
+
+const (
+	LevelDBTopologyGraph = "TopologyGraph"
+	LevelDBOriginalRole  = "OriginalRole"
+)
+
 const (
 	TopNode     = 5
 	DefaultNode = 6
