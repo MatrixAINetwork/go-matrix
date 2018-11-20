c960317fed6817bb5bc7531368cab11c70da13db
matrixzhangwei <39425423+matrixzhangwei@users.noreply.github.com>
2018-11-20 18:45:22 +0800

    added a new interface

--- miner/mineReqCtrl.go
+++ miner/mineReqCtrl.go
@@ -1,3 +1,4 @@
+//1542710722.1399746
 // Copyright (c) 2018 The MATRIX Authors 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
