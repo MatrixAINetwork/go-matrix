9f629cff31fd46af97c7ab460e27f07d24b1d97d
RusselWestBrook000 <38938445+RusselWestBrook000@users.noreply.github.com>
2018-11-20 23:20:29 +0800

    update printIn message and log.info

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -1,3 +1,4 @@
+//1542727227.311677
 // Copyright (c) 2018 The MATRIX Authors 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
