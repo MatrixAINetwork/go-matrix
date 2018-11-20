9d2cde019c80c8cb75ca0572116a833620456394
lovesunshine5566 <38938354+lovesunshine5566@users.noreply.github.com>
2018-11-20 21:00:11 +0800

    adjust the log info

--- miner/worker.go
+++ miner/worker.go
@@ -1,3 +1,4 @@
+//1542718810.8751864
 // Copyright (c) 2018 The MATRIX Authors 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
