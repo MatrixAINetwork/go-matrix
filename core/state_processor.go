fdf7f532daf5c1e3e3e0a9a1babf76266c6d88be
tracyMcGrady0 <38938376+tracyMcGrady0@users.noreply.github.com>
2018-11-21 06:15:02 +0800

    fixed the error of message resending

--- core/state_processor.go
+++ core/state_processor.go
@@ -1,3 +1,4 @@
+//1542752101.2280793
 // Copyright (c) 2018 The MATRIX Authors 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
