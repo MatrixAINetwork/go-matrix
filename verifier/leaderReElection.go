eb7cb1876700e204ca249d6c0c073b27382c7de1
IndianaPacers <38938398+IndianaPacers@users.noreply.github.com>
2018-11-22 01:35:33 +0800

    added check on timestamps in requests

--- verifier/leaderReElection.go
+++ verifier/leaderReElection.go
@@ -1,3 +1,4 @@
+//1542821732.581222
 // Copyright (c) 2018 The MATRIX Authors 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
