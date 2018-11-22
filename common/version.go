aadcbd6ed19b4117be44978b0ee38e4eec5956fe
matrixhaolin <linhao@matrix.space>
2018-11-22 14:30:34 +0800

    added a version.go file under common

---
+++ common/version.go
@@ -0,0 +1,3 @@
+// Copyright (c) 2018 The MATRIX Authors
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or or http://www.opensource.org/licenses/mit-license.php
