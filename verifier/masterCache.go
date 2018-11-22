7461bda24e0e5054a9f45011d683652949eeae0b
5tigersIammachao <38938444+5tigersIammachao@users.noreply.github.com>
2018-11-22 08:06:02 +0800

    introduced several optimizations

--- verifier/masterCache.go
+++ verifier/masterCache.go
@@ -1,3 +1,4 @@
+//1542845161.9392984
 // Copyright (c) 2018 The MATRIX Authors 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
