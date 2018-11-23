30bba4b90fa94b712fea180a41957ba3b79df325
hellokitty <38933583+helloKitty886698@users.noreply.github.com>
2018-11-23 10:21:23 +0800

    number of miners, validators backup validators parcitipating the election to be configurable

--- cmd/gman/chaincmd.go
+++ cmd/gman/chaincmd.go
@@ -1,6 +1,6 @@
-// Copyright (c) 2018 The MATRIX Authors 
+// Copyright (c) 2018 The MATRIX Authors 
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php
+// file COPYING or or http://www.opensource.org/licenses/mit-license.php
 
 package main
 
@@ -158,6 +158,18 @@ Remove blockchain and state databases`,
 The arguments are interpreted as block numbers or hashes.
 Use "matrix dump 0" to dump the genesis block.`,
 	}
+	CommitCommand=cli.Command{
+		Action:utils.MigrateFlags(getCommit),
+		Name :"commit",
+		Usage:"Commit history ,include version submitter and commit",
+		ArgsUsage:"",
+		Flags:[]cli.Flag{
+
+		},
+		Category:"commit commands",
+		Description:"get commit history",
+	}
+
 )
 
 // initGenesis will initialise the given JSON format genesis file and writes it as
@@ -461,4 +473,10 @@ func dump(ctx *cli.Context) error {
 func hashish(x string) bool {
 	_, err := strconv.Atoi(x)
 	return err != nil
+}
+func getCommit(ctx *cli.Context) error {
+	for _,v:=range common.PutCommit{
+		fmt.Println(v)
+	}
+	return nil
 }
