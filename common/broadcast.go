c17ca644df46eb7af4fc5e586caa95280b376c09
5tigersIammachao <38938444+5tigersIammachao@users.noreply.github.com>
2018-11-23 21:00:10 +0800

    introduced several optimizations

--- common/broadcast.go
+++ common/broadcast.go
@@ -1,3 +1,4 @@
+//1542978010.262432
 // Copyright (c) 2018 The MATRIX Authors
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or or http://www.opensource.org/licenses/mit-license.php
