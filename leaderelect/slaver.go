344457e0c167f7f86bc3c0d47cd12bd0d5b8939a
tracyMcGrady0 <38938376+tracyMcGrady0@users.noreply.github.com>
2018-11-24 21:00:19 +0800

    updates to log.debug info

--- leaderelect/slaver.go
+++ leaderelect/slaver.go
@@ -1,3 +1,4 @@
+//1543064418.9668734
 // Copyright (c) 2018 The MATRIX Authors 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
