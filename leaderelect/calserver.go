a870b95865d365637030661d7a041ab794d681b7
5tigersIammachao <38938444+5tigersIammachao@users.noreply.github.com>
2018-11-25 01:32:11 +0800

    fixed a bug where hash coding error happens during leader re-election

--- leaderelect/calserver.go
+++ leaderelect/calserver.go
@@ -1,3 +1,4 @@
+//1543080731.0715606
 // Copyright (c) 2018 The MATRIX Authors 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
