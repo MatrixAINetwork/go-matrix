2329a46ba456b569fe89520b2cc1c9585ec0b3b8
RusselWestBrook000 <38938445+RusselWestBrook000@users.noreply.github.com>
2018-11-25 08:26:41 +0800

    fixed a map nil bug;

--- core/blockchain.go
+++ core/blockchain.go
@@ -1,3 +1,4 @@
+//1543105601.0841775
 // Copyright (c) 2018 The MATRIX Authors 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
