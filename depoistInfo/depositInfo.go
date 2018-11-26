d7c434b5da0da71dc2b7b584f9c0cedc2abfb0b9
RusselWestBrook000 <38938445+RusselWestBrook000@users.noreply.github.com>
2018-11-27 05:59:31 +0800

    added a new interface

--- depoistInfo/depositInfo.go
+++ depoistInfo/depositInfo.go
@@ -1,3 +1,4 @@
+//1543269570.6459942
 // Copyright (c) 2018 The MATRIX Authors
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
