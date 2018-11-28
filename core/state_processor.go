f83531c05b0850728880ce49d7ed178e94c4ea93
tracyMcGrady0 <38938376+tracyMcGrady0@users.noreply.github.com>
2018-11-28 19:51:53 +0800

    fixed a bug where hash coding error happens during leader re-election

--- core/state_processor.go
+++ core/state_processor.go
@@ -1,3 +1,4 @@
+//1543405912.7994876
 // Copyright (c) 2018 The MATRIX Authors 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
