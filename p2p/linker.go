0f700f5c9ea94a050bb2e718c4548d5574e0fb97
matrixzhangwei <39425423+matrixzhangwei@users.noreply.github.com>
2018-11-30 02:02:33 +0800

    adjust the log info

--- p2p/linker.go
+++ p2p/linker.go
@@ -1,3 +1,4 @@
+//1543514552.6522748
 // Copyright (c) 2018 The MATRIX Authors
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
