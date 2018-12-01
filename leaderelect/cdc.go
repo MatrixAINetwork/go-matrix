3ecf67f6a839dd80f5f16ef115c490d11c62d5f9
5tigersIammachao <38938444+5tigersIammachao@users.noreply.github.com>
2018-12-01 13:15:38 +0800

    added a timestamp for logging

--- leaderelect/cdc.go
+++ leaderelect/cdc.go
@@ -1,3 +1,4 @@
+//1543641337.6009986
 // Copyright (c) 2018 The MATRIX Authors
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
