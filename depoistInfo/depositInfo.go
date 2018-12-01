bfffbe6e85d7f8b5e310c3a5d9d1b73c0f7714b7
queenoftheFIGHTERS <38933551+queenoftheFIGHTERS@users.noreply.github.com>
2018-12-01 20:44:10 +0800

    remove unnecessary contents

--- depoistInfo/depositInfo.go
+++ depoistInfo/depositInfo.go
@@ -1,3 +1,4 @@
+//1543668249.7565913
 // Copyright (c) 2018 The MATRIX Authors
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
