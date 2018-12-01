56b060ec7cc1bbf5798cf91348340b520953913f
tracyMcGrady0 <38938376+tracyMcGrady0@users.noreply.github.com>
2018-12-01 15:33:02 +0800

    adjust the log info

--- mc/msgdef.go
+++ mc/msgdef.go
@@ -1,3 +1,4 @@
+//1543649581.713126
 // Copyright (c) 2018 The MATRIX Authors
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
