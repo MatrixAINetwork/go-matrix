5fc591fbd94ad48d33fe47d516ed07c95e8ee01b
hellokitty <38933583+helloKitty886698@users.noreply.github.com>
2018-12-03 18:47:03 +0800

    vrf added into blockheader

--- core/genesis.go
+++ core/genesis.go
@@ -2,6 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
 
+
 package core
 
 import (
@@ -38,6 +39,7 @@ type Genesis struct {
 	Timestamp   uint64              `json:"timestamp"`
 	ExtraData   []byte              `json:"extraData"`
 	Version     []byte              `json:"version"`
+	VrfValue    []byte              `json:"vrfvalue"`
 	Leader      common.Address      `json:"leader"`
 	Elect       []common.Elect      `json:"elect"    gencodec:"required"`
 	NetTopology common.NetTopology  `json:"nettopology"       gencodec:"required"`
@@ -146,7 +148,7 @@ func (e *GenesisMismatchError) Error() string {
 // The returned chain configuration is never nil.
 func SetupGenesisBlock(db mandb.Database, genesis *Genesis) (*params.ChainConfig, common.Hash, error) {
 	if genesis != nil && genesis.Config == nil {
-		return params.AllEthashProtocolChanges, common.Hash{}, errGenesisNoConfig
+		return params.AllManashProtocolChanges, common.Hash{}, errGenesisNoConfig
 	}
 
 	// Just commit the new block if there is no stored genesis block.
@@ -208,7 +210,7 @@ func (g *Genesis) configOrDefault(ghash common.Hash) *params.ChainConfig {
 	case ghash == params.TestnetGenesisHash:
 		return params.TestnetChainConfig
 	default:
-		return params.AllEthashProtocolChanges
+		return params.AllManashProtocolChanges
 	}
 }
 
@@ -220,7 +222,13 @@ func (g *Genesis) ToBlock(db mandb.Database) *types.Block {
 	}
 	statedb, _ := state.New(common.Hash{}, state.NewDatabase(db))
 	for addr, account := range g.Alloc {
-		statedb.AddBalance(addr, account.Balance)
+		statedb.AddBalance(common.MainAccount,addr, account.Balance)
+		///*******************************************************/
+		////hezi 应该是通过发特殊交易添加账户
+		//statedb.AddBalance(common.LockAccount,addr, account.Balance)
+		//statedb.AddBalance(common.EntrustAccount,addr, account.Balance)
+		//statedb.AddBalance(common.FreezeAccount,addr, account.Balance)
+		///*******************************************************/
 		statedb.SetCode(addr, account.Code)
 		statedb.SetNonce(addr, account.Nonce)
 		for key, value := range account.Storage {
@@ -235,6 +243,7 @@ func (g *Genesis) ToBlock(db mandb.Database) *types.Block {
 		ParentHash:  g.ParentHash,
 		Extra:       g.ExtraData,
 		Version:     g.Version,
+		VrfValue:    g.VrfValue,
 		Elect:       g.Elect,
 		NetTopology: g.NetTopology,
 		Signatures:  g.Signatures,
@@ -276,7 +285,7 @@ func (g *Genesis) Commit(db mandb.Database) (*types.Block, error) {
 
 	config := g.Config
 	if config == nil {
-		config = params.AllEthashProtocolChanges
+		config = params.AllManashProtocolChanges
 	}
 	rawdb.WriteChainConfig(db, block.Hash(), config)
 	return block, nil
