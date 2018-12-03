041781cad5a6556b77978f13a874b886f4665c84
5tigersIammachao <38938444+5tigersIammachao@users.noreply.github.com>
2018-12-03 10:22:53 +0800

    vrf added into blockheader

--- blkverify/blkverify/verify_election_topology.go
+++ blkverify/blkverify/verify_election_topology.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 The MATRIX Authors 
+// Copyright (c) 2018 The MATRIX Authors
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
 package blkverify
@@ -9,8 +9,13 @@ import (
 	"github.com/matrix/go-matrix/core/types"
 	"github.com/matrix/go-matrix/log"
 	"github.com/matrix/go-matrix/mc"
-	"github.com/matrix/go-matrix/topnode"
+	"github.com/matrix/go-matrix/olconsensus"
 	"github.com/pkg/errors"
+	"github.com/btcsuite/btcd/btcec"
+	"crypto/ecdsa"
+	"github.com/matrix/go-matrix/baseinterface"
+	"github.com/matrix/go-matrix/crypto"
+	"encoding/json"
 )
 
 var (
@@ -53,6 +58,57 @@ func (p *Process) verifyNetTopology(header *types.Header) error {
 
 	return p.verifyChgNetTopology(header)
 }
+func (p *Process)verifyVrf(header *types.Header)error{
+	log.Error("vrf","len header.VrfValue",len(header.VrfValue),"data",header.VrfValue,"高度",header.Number.Uint64())
+	account,_,_:=common.GetVrfInfoFromHeader(header.VrfValue)
+
+	log.Error("vrf","从区块头重算出账户户",account,"高度",header.Number.Uint64())
+
+	public:=account
+	curve := btcec.S256()
+	pk1, err := btcec.ParsePubKey(public, curve)
+	if err != nil {
+		log.Error("vrf转换失败","err",err,"account",account,"len",len(account))
+		return err
+	}
+
+	pk1_1 := (*ecdsa.PublicKey)(pk1)
+	_,vrfValue,vrfProof:=common.GetVrfInfoFromHeader(header.VrfValue)
+
+	preBlock:=p.blockChain().GetBlockByHash(header.ParentHash)
+	if preBlock==nil{
+		return errors.New("获取父区块失败")
+	}
+	_,preVrfValue,preVrfProof:=common.GetVrfInfoFromHeader(preBlock.Header().VrfValue)
+
+	preMsg:=common.VrfMsg{
+		VrfValue:preVrfValue,
+		VrfProof:preVrfProof,
+		Hash:header.ParentHash,
+	}
+
+	preVrfMsg,err:=json.Marshal(preMsg)
+	if err!=nil{
+		log.Error(p.logExtraInfo(),"生成vefmsg出错",err,"parentMsg",preVrfMsg)
+		return errors.New("生成vrfmsg出错")
+	}else{
+		log.Error("生成vrfmsg成功")
+	}
+	//log.Info("msgggggvrf_verify","preVrfMsg",preVrfMsg,"高度",header.Number.Uint64(),"VrfProof",preMsg.VrfProof,"VrfValue",preMsg.VrfValue,"Hash",preMsg.Hash)
+	if err:=baseinterface.NewVrf().VerifyVrf(pk1_1,preVrfMsg,vrfValue,vrfProof);err!=nil{
+		log.Error("vrf verify ","err",err)
+		return err
+	}
+
+	ans:=crypto.PubkeyToAddress(*pk1_1)
+	if ans.Equal(header.Leader){
+		log.Error("vrf leader comparre","与leader不匹配","nil")
+		return nil
+	}
+	return errors.New("公钥与leader账户不匹配")
+
+
+}
 
 func (p *Process) verifyAllNetTopology(header *types.Header) error {
 	info, err := p.reElection().GetNetTopologyAll(header.ParentHash)
@@ -88,22 +144,48 @@ func (p *Process) verifyChgNetTopology(header *types.Header) error {
 	// get online and offline info from header and prev topology
 	offlineTopNodes, onlinePrimaryNods, offlinePrimaryNodes := p.parseOnlineState(header, prevTopology)
 
-	// get local consensus on-line state
-	curTopNodeState, primaryNodeState := p.topNode().GetConsensusOnlineState()
-	//check state from header with state in local consensus
-	if false == p.checkStateByConsensus(offlineTopNodes, nil, curTopNodeState) {
-		return errTopNodeState
+	log.INFO("scfffff-verify", "header.NetTop", header.NetTopology, "高度", header.Number.Uint64())
+	log.INFO("scfffff--verify", "prevTopology", prevTopology)
+	log.INFO("scfffff--verify", "offlineTopNodes", offlineTopNodes)
+	log.INFO("scfffff--verify", "onlinePrimaryNods", onlinePrimaryNods)
+	log.INFO("scfffff--verify", "offlinePrimaryNodes", offlinePrimaryNodes)
+
+	originTopology, err := ca.GetTopologyByHash(common.RoleValidator|common.RoleBackupValidator|common.RoleMiner|common.RoleBackupMiner, header.ParentHash)
+	if err != nil {
+		return nil
 	}
-	if false == p.checkStateByConsensus(offlinePrimaryNodes, onlinePrimaryNods, primaryNodeState) {
-		return errPrimaryNodeState
+	originTopNodes := make([]common.Address, 0)
+	for _, node := range originTopology.NodeList {
+		originTopNodes = append(originTopNodes, node.Account)
+		log.Info(p.logExtraInfo(), "originTopNode", node.Account)
+	}
+	onlineElectNodes := make([]common.Address, 0)
+	for _, node := range originTopology.ElectList {
+		onlineElectNodes = append(onlineElectNodes, node.Account)
+		log.Info(p.logExtraInfo(), "onlineElectNodes", node.Account)
+
+	}
+	electNumber:=common.GetLastReElectionNumber(p.number)
+	if electNumber > 0 {
+		electNumber -= 1
+	}
+	p.pm.topNode.SetElectNodes(originTopNodes, electNumber)
+
+	if false == p.topNode().CheckAddressConsensusOnlineState(offlineTopNodes, onlinePrimaryNods, offlinePrimaryNodes) {
+		return errTopNodeState
 	}
 
 	// generate topology alter info
+	log.INFO("scffffff---Verify---GetTopoChange start ", "p.number", p.number, "offlineTopNodes", offlineTopNodes, "onlinePrimaryNods", onlinePrimaryNods)
 	alterInfo, err := p.reElection().GetTopoChange(header.ParentHash, offlineTopNodes)
+	log.INFO("scffffff---Verify---GetTopoChange end", "alterInfo", alterInfo, "err", err)
 	if err != nil {
 		return err
 	}
 
+	for _, value := range alterInfo {
+		log.Info(p.logExtraInfo(), "alter-A", value.A, "position", value.Position)
+	}
 	// generate self net topology
 	netTopology := p.reElection().TransferToNetTopologyChgStu(alterInfo, onlinePrimaryNods, offlinePrimaryNodes)
 	if len(netTopology.NetTopologyData) != len(header.NetTopology.NetTopologyData) {
@@ -120,13 +202,13 @@ func (p *Process) verifyChgNetTopology(header *types.Header) error {
 
 func (p *Process) checkStateByConsensus(offlineNodes []common.Address,
 	onlineNodes []common.Address,
-	stateMap map[common.Address]topnode.OnlineState) bool {
+	stateMap map[common.Address]olconsensus.OnlineState) bool {
 
 	for _, offlineNode := range offlineNodes {
 		if consensusState, OK := stateMap[offlineNode]; OK == false {
 			log.ERROR(p.logExtraInfo(), "拓扑变化检测(通过本地共识状态), 本地共识状态未找到, node", offlineNode, "区块请求中的状态", "下线")
 			return false
-		} else if consensusState != topnode.Offline {
+		} else if consensusState != olconsensus.Offline {
 			log.Warn(p.logExtraInfo(), "拓扑变化检测(通过本地共识状态), 本地共识状态不匹配, node", offlineNode, "区块请求中的状态", "下线")
 			return false
 		}
@@ -136,7 +218,7 @@ func (p *Process) checkStateByConsensus(offlineNodes []common.Address,
 		if consensusState, OK := stateMap[onlineNode]; OK == false {
 			log.Warn(p.logExtraInfo(), "拓扑变化检测(通过本地共识状态), 本地共识状态未找到, node", onlineNode, "区块请求中的状态", "上线")
 			return false
-		} else if consensusState != topnode.Online {
+		} else if consensusState != olconsensus.Online {
 			log.Warn(p.logExtraInfo(), "拓扑变化检测(通过本地共识状态), 本地共识状态不匹配, node", onlineNode, "区块请求中的状态", "上线")
 			return false
 		}
