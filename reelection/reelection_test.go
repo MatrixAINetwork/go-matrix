94b30a0826ab571d15628dc3e66f3ab7183de06e
queenoftheFIGHTERS <38933551+queenoftheFIGHTERS@users.noreply.github.com>
2018-12-05 06:09:50 +0800

    run tests on reelection after vrf added into blockheader ?

--- reelection/reelection_test.go
+++ reelection/reelection_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 The MATRIX Authors 
+// Copyright (c) 2018 The MATRIX Authors
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
 package reelection
@@ -7,15 +7,12 @@ import (
 	"fmt"
 	"math/big"
 	"testing"
-	"time"
-
-	"github.com/matrix/go-matrix/log"
-	"github.com/matrix/go-matrix/random"
 
 	"github.com/matrix/go-matrix/common"
-	"github.com/matrix/go-matrix/mc"
-
-	"github.com/matrix/go-matrix/man"
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"log"
 )
 
 //func Post() {
@@ -38,8 +35,8 @@ import (
 //	log.Info("electseed", electseed)
 //	log.Info("seed err", err)
 //
-//	var man *man.Matrix
-//	reElect, err := New(man)
+//	var eth *eth.Ethereum
+//	reElect, err := New(eth)
 //	log.Info("err", err)
 //
 //	go Post()
@@ -62,4 +59,64 @@ func TestT(t *testing.T) {
 func TestCase(t *testing.T) {
 	ans1, ans2 := GetAllElectedByHeight(big.NewInt(100), common.RoleMiner)
 	fmt.Println(ans1, ans2)
+}
+
+func TestNew1(t *testing.T) {
+	info_temp:=Info{
+		Position:1,
+		Account:common.BigToAddress(big.NewInt(100)),
+	}
+	info:=[]Info{}
+	info=append(info,info_temp)
+	node:=NodeSupport{}
+	node.First=append(node.First,info[0:]...)
+	node.Second=append(node.Second,info[0:]...)
+	node.Third=append(node.Third,info[0:]...)
+
+
+	marshalData, err := json.Marshal(node)
+	if err != nil {
+		fmt.Println("测试支持", "Marshal失败 data", node)
+
+	}
+	err = ioutil.WriteFile("./test.json", marshalData, os.ModeAppend)
+	if err != nil {
+		fmt.Println("测试支持", "生成test文件成功")
+	}
+
+
+}
+type JsonConfig struct {
+	First []Info
+	Second []Info
+	Third []Info
+}
+
+type JsonStruct struct {
+}
+
+func NewJsonStruct() *JsonStruct {
+	return &JsonStruct{}
+}
+
+func (jst *JsonStruct) Load(filename string, v interface{}) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Println("读取通用配置文件失败 err", err, "file", filename)
+		os.Exit(-1)
+		return
+	}
+	err = json.Unmarshal(data, v)
+	if err != nil {
+		log.Println("通用配置文件数据获取失败 err", err)
+		os.Exit(-1)
+		return
+	}
+}
+
+func Test111(t *testing.T){
+	JsonParse := NewJsonStruct()
+	v := JsonConfig{}
+	JsonParse.Load("./test.json", &v)
+	fmt.Println("ans",v)
 }
