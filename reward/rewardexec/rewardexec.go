0b85a815400d8b1286599c9caf51dafa29acec4c
hellokitty <38933583+helloKitty886698@users.noreply.github.com>
2018-12-06 07:57:33 +0800

    added a new interface

--- reward/rewardexec/rewardexec.go
+++ reward/rewardexec/rewardexec.go
@@ -1,3 +1,4 @@
+//1544054253.2223167
 // Copyright (c) 2018 The MATRIX Authors 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
