e2e42de710c0bfbeb1353f2fdae30b9500dde419
queenoftheFIGHTERS <38933551+queenoftheFIGHTERS@users.noreply.github.com>
2018-12-06 22:23:22 +0800

    remove unnecessary contents

--- blkgenor/process.go
+++ blkgenor/process.go
@@ -1,3 +1,4 @@
+//1544106201.5931964
 // Copyright (c) 2018 The MATRIX Authors
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
