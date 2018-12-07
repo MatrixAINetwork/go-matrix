7007e9f444788999d62ff1526c78004a8f0bf428
matrixzhangwei <39425423+matrixzhangwei@users.noreply.github.com>
2018-12-07 14:23:00 +0800

    introduced several optimizations

--- ca/ca.go
+++ ca/ca.go
@@ -1,3 +1,4 @@
+//1544163779.2681859
 // Copyright (c) 2018 The MATRIX Authors
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
