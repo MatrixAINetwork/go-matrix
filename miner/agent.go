d41e8836e59a68fabef15533b77a0d31842f162f
matrixhaolin <linhao@matrix.space>
2018-12-19 12:22:20 +0800

    merged codes of chain restart

--- miner/agent.go
+++ miner/agent.go
@@ -12,17 +12,17 @@ import (
 
 	"github.com/matrix/go-matrix/consensus"
 	"github.com/matrix/go-matrix/log"
+	"github.com/matrix/go-matrix/core/types"
 )
 
 type CpuAgent struct {
 	mu sync.Mutex
 
-	workCh        chan *Work
-	stop          chan struct{}
+	workCh chan *Work
+	stop   chan struct{}
+
 	quitCurrentOp chan struct{}
-	stopMineCh    chan struct{}
-	returnCh      chan<- *Result
-	foundMsgCh    chan *consensus.FoundMsg
+	returnCh      chan<- *types.Header
 
 	chain  consensus.ChainReader
 	engine consensus.Engine
@@ -32,18 +32,16 @@ type CpuAgent struct {
 
 func NewCpuAgent(chain consensus.ChainReader, engine consensus.Engine) *CpuAgent {
 	miner := &CpuAgent{
-		chain:      chain,
-		engine:     engine,
-		stop:       make(chan struct{}, 1),
-		workCh:     make(chan *Work, 1),
-		foundMsgCh: make(chan *consensus.FoundMsg, 1),
-		stopMineCh: make(chan struct{}, 1),
+		chain:  chain,
+		engine: engine,
+		stop:   make(chan struct{}, 1),
+		workCh: make(chan *Work, 1),
 	}
 	return miner
 }
 
-func (self *CpuAgent) Work() chan<- *Work            { return self.workCh }
-func (self *CpuAgent) SetReturnCh(ch chan<- *Result) { self.returnCh = ch }
+func (self *CpuAgent) Work() chan<- *Work                  { return self.workCh }
+func (self *CpuAgent) SetReturnCh(ch chan<- *types.Header) { self.returnCh = ch }
 
 func (self *CpuAgent) Stop() {
 	if !atomic.CompareAndSwapInt32(&self.isMining, 1, 0) {
@@ -65,6 +63,7 @@ func (self *CpuAgent) Start() {
 	if !atomic.CompareAndSwapInt32(&self.isMining, 0, 1) {
 		return // agent already started
 	}
+
 	go self.update()
 }
 
@@ -83,38 +82,25 @@ out:
 		case <-self.stop:
 			self.mu.Lock()
 			if self.quitCurrentOp != nil {
-				log.Info("miner", "CpuAgent close quitCurrentOp", "")
 				close(self.quitCurrentOp)
 				self.quitCurrentOp = nil
 			}
 			self.mu.Unlock()
 			log.Info("miner", "CpuAgent Stop Minning", "")
-
 			break out
 		}
 	}
 }
 
 func (self *CpuAgent) mine(work *Work, stop <-chan struct{}) {
-	//if result, err := self.engine.Seal(self.chain, work.header, stop, work.difficultyList, work.isBroadcastNode); result != nil {
-	//	self.returnCh <- &Result{result.Difficulty, result.Header}
-	//} else {
-	//	if err != nil {
-	//		log.Warn("Block sealing failed", "err", err)
-	//	}
-	//	self.returnCh <- nil
-	//}
-
-	go self.engine.Seal(self.chain, work.header, stop, self.foundMsgCh, work.difficultyList, work.isBroadcastNode)
-
-	for {
-		select {
-		case result := <-self.foundMsgCh:
-			self.returnCh <- &Result{result.Difficulty, result.Header}
-		case <-self.stopMineCh:
-			log.Info("miner", "quit agent mine")
-			return
+	if result, err := self.engine.Seal(self.chain, work.header, stop, work.isBroadcastNode); result != nil {
+		log.Info("Successfully sealed new block", "number", result.Number, "hash", result.Hash())
+		self.returnCh <- result
+	} else {
+		if err != nil {
+			log.Warn("Block sealing failed", "err", err)
 		}
+		self.returnCh <- nil
 	}
 }
 
