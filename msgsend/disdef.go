bf31ef088161d0ee8f44c6faaea035916ee09397
matrixhaolin <linhao@matrix.space>
2018-12-19 20:08:41 +0800

    minor change to disdef.go

--- msgsend/disdef.go
+++ msgsend/disdef.go
@@ -20,7 +20,7 @@ type NetData struct {
 	Msg     []byte
 }
 
-type BroadcastRspMsgForMarshal struct {
+type fullBlockMsgForMarshal struct {
 	Header *types.Header
 	Txs    []*types.Transaction_Mx
 }
