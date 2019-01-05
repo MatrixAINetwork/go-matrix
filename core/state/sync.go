7ab992812424ff649a27f1e0af27a144d72c4d3a
matrixhaolin <linhao@matrix.space>
2019-01-05 14:36:47 +0800

    Update sync.go for fastSyncMode

--- core/state/sync.go
+++ core/state/sync.go
@@ -18,7 +18,8 @@ func NewStateSync(root common.Hash, database trie.DatabaseReader) *trie.TrieSync
 	callback := func(leaf []byte, parent common.Hash) error {
 		var obj Account
 		if err := rlp.Decode(bytes.NewReader(leaf), &obj); err != nil {
-			return err
+			//return err
+			return nil
 		}
 		syncer.AddSubTrie(obj.Root, 64, parent, nil)
 		syncer.AddRawEntry(common.BytesToHash(obj.CodeHash), 64, parent)
