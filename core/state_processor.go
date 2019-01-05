10372c304fbd7f53e73897e5120fae0cbf6d314b
matrixhaolin <linhao@matrix.space>
2019-01-05 10:49:52 +0800

    miner跑交易增加委托gas判断，防止state不一致

--- core/state_processor.go
+++ core/state_processor.go
@@ -1,6 +1,6 @@
-// Copyright (c) 2018 The MATRIX Authors
+// Copyright (c) 2018 The MATRIX Authors
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php
+// file COPYING or or http://www.opensource.org/licenses/mit-license.php
 
 package core
 
@@ -24,6 +24,7 @@ import (
 	"math/big"
 	"runtime"
 	"sync"
+	"time"
 )
 
 // StateProcessor is a basic Processor, which takes care of transitioning
@@ -180,6 +181,27 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 			tmpstxs = append(tmpstxs, stxs...)
 			stxs = tmpstxs
 			continue
+		}
+		if tx.IsEntrustTx() {
+			from := tx.From()
+			entrustFrom := statedb.GetGasAuthFrom(from, p.bc.CurrentBlock().NumberU64()) //
+			if !entrustFrom.Equal(common.Address{}) {
+				tx.Setentrustfrom(entrustFrom)
+				//tx.IsEntrustGas = true
+				tx.SetIsEntrustGas(true)
+			} else {
+				entrustFrom := statedb.GetGasAuthFromByTime(from, uint64(time.Now().Unix()))
+				if !entrustFrom.Equal(common.Address{}) {
+					tx.Setentrustfrom(entrustFrom)
+					//tx.IsEntrustGas = true
+					//tx.IsEntrustByTime = true
+					tx.SetIsEntrustGas(true)
+					tx.SetIsEntrustByTime(true)
+				} else {
+					log.Error("该用户没有被授权过委托Gas")
+					return nil, nil, 0, ErrWithoutAuth
+				}
+			}
 		}
 		statedb.Prepare(tx.Hash(), block.Hash(), i)
 		receipt, _, err := ApplyTransaction(p.config, p.bc, nil, gp, statedb, header, tx, usedGas, cfg)
