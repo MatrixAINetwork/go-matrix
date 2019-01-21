8a26f3f51769c2e87c5f30d5f127e1e218f5c4b1
matrixhaolin <linhao@matrix.space>
2019-01-21 17:18:35 +0800

    added a req_cache_test file

---
+++ blkverify/req_cache_test.go
@@ -0,0 +1,28 @@
+package blkverify
+
+import (
+	"github.com/matrix/go-matrix/common"
+	"github.com/matrix/go-matrix/core/types"
+	"github.com/matrix/go-matrix/log"
+	"github.com/matrix/go-matrix/mc"
+	"math/big"
+	"testing"
+)
+
+func TestReqCacheSort(t *testing.T) {
+	log.InitLog(3)
+
+	cache := make([]*reqData, 0)
+	cache = append(cache, &reqData{reqType: reqTypeUnknownReq, hash: common.HexToHash("0x0001"), req: &mc.HD_BlkConsensusReqMsg{Header: &types.Header{Time: big.NewInt(11)}}})
+	cache = append(cache, &reqData{reqType: reqTypeUnknownReq, hash: common.HexToHash("0x0002"), req: &mc.HD_BlkConsensusReqMsg{Header: &types.Header{Time: big.NewInt(11)}}})
+	cache = append(cache, &reqData{reqType: reqTypeLeaderReq, hash: common.HexToHash("0x0003"), req: &mc.HD_BlkConsensusReqMsg{Header: &types.Header{Time: big.NewInt(13)}}})
+	cache = append(cache, &reqData{reqType: reqTypeUnknownReq, hash: common.HexToHash("0x0004"), req: &mc.HD_BlkConsensusReqMsg{Header: &types.Header{Time: big.NewInt(14)}}})
+	cache = append(cache, &reqData{reqType: reqTypeUnknownReq, hash: common.HexToHash("0x0005"), req: &mc.HD_BlkConsensusReqMsg{Header: &types.Header{Time: big.NewInt(15)}}})
+	cache = append(cache, &reqData{reqType: reqTypeLocalReq, hash: common.HexToHash("0x0006"), req: &mc.HD_BlkConsensusReqMsg{Header: &types.Header{Time: big.NewInt(11)}}})
+
+	cache = delBadReqAndSort(cache, true)
+
+	for i, req := range cache {
+		log.Info("cache", "index", i, "req type", req.reqType, "time", req.req.Header.Time, "hash", req.hash.Big())
+	}
+}
