99a2c64ce4804fe719a369d0ee7ff8c89b9d1308
matrixhaolin <linhao@matrix.space>
2019-01-21 15:01:40 +0800

    minor rule changes to MATRIX account address

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -9,6 +9,7 @@ import (
 	"math/big"
 
 	matrix "github.com/matrix/go-matrix"
+	"github.com/matrix/go-matrix/base58"
 	"github.com/matrix/go-matrix/common"
 	"github.com/matrix/go-matrix/core/types"
 	"github.com/matrix/go-matrix/event"
@@ -17,9 +18,13 @@ import (
 // Account represents an Matrix account located at a specific location defined
 // by the optional URL field.
 type Account struct {
-	Address    common.Address `json:"address"`    // Matrix account address derived from the key
-	ManAddress string         `json:"manAddress"` // hezi
-	URL        URL            `json:"url"`        // Optional resource locator within a backend
+	Address common.Address `json:"address"` // Matrix account address derived from the key
+	//	ManAddress string         `json:"manAddress"` //
+	URL URL `json:"url"` // Optional resource locator within a backend
+}
+
+func (Ac *Account) ManAddress() string {
+	return base58.Base58EncodeToString("MAN", Ac.Address)
 }
 
 // Wallet represents a software or hardware wallet that might contain one or more
