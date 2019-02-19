5f09b663202e780a43adb397494eb7df57410cb4
matrixhaolin <linhao@matrix.space>
2019-02-19 18:02:29 +0800

    remove garbage character in the file header

--- consensus/cryptonight/keccak.go
+++ consensus/cryptonight/keccak.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2018-2019 The MATRIX Authors
+// Copyright (c) 2018-2019 The MATRIX Authors
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
 // Copyright 2017-2018 DERO Project. All rights reserved.
