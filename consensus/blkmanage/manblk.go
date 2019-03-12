0744a0b76e446d8c3ce5d0d8c4f38729b71d4a86
matrixhaolin <linhao@matrix.space>
2019-03-12 09:48:40 +0800

    unified the file header

--- consensus/blkmanage/manblk.go
+++ consensus/blkmanage/manblk.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2018-2019 The MATRIX Authors
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php
+
 package blkmanage
 
 import (
