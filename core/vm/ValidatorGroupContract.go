b54d92e083a7ae134adf978d0c4af498d48d78ee
MatrixAINetworkMan <github@matrix.space>
2019-06-21 13:23:38 +0800

    update

---
+++ core/vm/ValidatorGroupContract.go
@@ -0,0 +1,289 @@
+// Copyright (c) 2018 The MATRIX Authors
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php
+package vm
+
+import (
+	"github.com/MatrixAINetwork/go-matrix/common"
+	"math/big"
+	"github.com/MatrixAINetwork/go-matrix/crypto"
+	"github.com/MatrixAINetwork/go-matrix/core/vm/validatorGroup"
+	"github.com/MatrixAINetwork/go-matrix/params"
+	"github.com/MatrixAINetwork/go-matrix/accounts/abi"
+	"github.com/MatrixAINetwork/go-matrix/rlp"
+	"errors"
+)
+var (
+	manBase = big.NewInt(1e18)
+	rateDecmal = 1.0e9
+	rateDecmalBig = big.NewInt(1e9)
+
+	developContractAddress = common.BytesToAddress([]byte{10})
+	ValidatorGroupContractAddress = common.BytesToAddress([]byte{20})
+	depLvel = []*big.Int{big.NewInt(0),
+		new(big.Int).Mul(big.NewInt(10000),manBase),
+		new(big.Int).Mul(big.NewInt(100000),manBase)}
+	errArguments = errors.New("Input Arguments Error")
+	errExpired = errors.New("Validator Group is Expired")
+	errNotExist = errors.New("Validator Account is not exist")
+	errOwnerInsufficient = errors.New("Owner Deposit Amount is insufficient")
+	errNotOwner = errors.New("Tx sender is not Owner")
+	errInsufficient = errors.New("insufficient balance for Refund")
+
+)
+
+type MethodInterface interface {
+	PrecompiledContract
+	MethodName()string
+	MethodID()[4]byte
+}
+type BaseMethod struct {
+	Name string
+	Abi *abi.ABI
+	GasUsed uint64
+	run func (input []byte, contract *Contract, evm *EVM) ([]byte, error)
+}
+func (bm* BaseMethod)Inputs()abi.Arguments{
+	return bm.Abi.Methods[bm.Name].Inputs
+}
+func (bm* BaseMethod)Outputs()abi.Arguments{
+	return bm.Abi.Methods[bm.Name].Outputs
+}
+func (bm* BaseMethod)MethodName()string{
+	return bm.Name
+}
+/*
+func (bm* BaseMethod)getFuncID(abi *abi.ABI,funcName string)[4]byte{
+	funId := abi.Methods[funcName].Id()
+	var ID [4]byte
+	copy(ID[:],funId[:4])
+	return ID
+}
+*/
+func (bm* BaseMethod)MethodID()[4]byte{
+	if len(bm.Name)==0{
+		return [4]byte{}
+	}
+	if method,exist := bm.Abi.Methods[bm.Name];exist{
+		funId := method.Id()
+		var ID [4]byte
+		copy(ID[:],funId[:4])
+		return ID
+	}
+	return [4]byte{}
+}
+func (bm* BaseMethod)RequiredGas(input []byte)uint64{
+	return bm.GasUsed
+}
+func (bm* BaseMethod)Run(input []byte, contract *Contract, evm *EVM) ([]byte, error){
+	return bm.run(input,contract,evm)
+}
+type BaseContract struct {
+	anonymousMethod MethodInterface
+//	constructMethod MethodInterface
+	methodMap map[[4]byte]MethodInterface
+}
+func (bc* BaseContract)AddMethod(md MethodInterface) {
+	bc.methodMap[md.MethodID()] = md
+}
+func (bc* BaseContract)GetMethod(input []byte)PrecompiledContract{
+	if len(input) == 0 {
+		return bc.anonymousMethod
+	}
+	if len(input)<4{
+		return nil
+	}
+	var ID [4]byte
+	copy(ID[:],input[:4])
+	return bc.methodMap[ID]
+}
+func (bc* BaseContract)RequiredGas(input []byte) uint64 {
+	method := bc.GetMethod(input)
+	if method!= nil{
+		return method.RequiredGas(input)
+	}
+	return params.TxGasContractCreation * 2
+}
+func (bc* BaseContract)Run(input []byte, contract *Contract, evm *EVM) ([]byte, error) {
+	method := bc.GetMethod(input)
+	if method!= nil{
+		return method.Run(input,contract,evm)
+	}
+	return nil,errExecutionReverted
+
+}
+
+func EmergeKey(address common.Address,surffix string)common.Hash{
+	hashKey := common.Hash{}
+	copy(hashKey[:],address[:])
+	copy(hashKey[20:],[]byte(surffix))
+	return hashKey
+}
+type ValidatorContractState struct {
+	childGroup validatorGroup.AddressSlice
+}
+func (vc *ValidatorContractState)RemoveEmptyValidatorGroup(address common.Address,state StateDBManager)error{
+	err := vc.GetState(ValidatorGroupContractAddress,state)
+	if err != nil{
+		return err
+	}
+	vc.childGroup.Remove(address)
+	return vc.SetState(ValidatorGroupContractAddress,state)
+//	return nil
+}
+func (vc *ValidatorContractState)Find(address common.Address)bool{
+	_,exist := vc.childGroup.Find(address)
+	return exist
+}
+func (vc *ValidatorContractState)Insert(address common.Address){
+	vc.childGroup.Insert(address)
+}
+func (vc *ValidatorContractState)SetState(contractAddress common.Address,state StateDBManager)error {
+	data,err := rlp.EncodeToBytes([]common.Address(vc.childGroup))
+	if err != nil {
+		return err
+	}
+	state.SetStateByteArray(params.MAN_COIN,contractAddress,EmergeKey(contractAddress,"ValiList"),data)
+	return nil
+}
+func (vc *ValidatorContractState)GetState(contractAdress common.Address,state StateDBManager)error {
+	data := state.GetStateByteArray(params.MAN_COIN,contractAdress,EmergeKey(contractAdress,"ValiList"))
+	if len(data) == 0 {
+		vc.childGroup = validatorGroup.AddressSlice{}
+		return nil
+	}
+	err := rlp.DecodeBytes(data,&vc.childGroup)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (vc *ValidatorContractState)GetValidatorGroupInfo(time uint64,state StateDBManager)(map[common.Address]*ValidatorGroupState,error){
+	err := vc.GetState(ValidatorGroupContractAddress,state)
+	if err != nil{
+		return nil,err
+	}
+	validatorGroupMap := make(map[common.Address]*ValidatorGroupState)
+	for _,addr := range vc.childGroup {
+		states := NewValidatorGroupState()
+		err := states.GetState(addr,time,state)
+		if err != nil{
+			return nil,err
+		}
+		validatorGroupMap[addr] = states
+	}
+	return validatorGroupMap,nil
+}
+//
+type ValidatorGroupContract struct {
+	BaseContract
+	childs ValidatorContractState
+}
+func NewTransInterestsInterface()TransInterestsInterface{
+	return NewValidatorGroupContract()
+}
+func NewValidatorGroupContract()*ValidatorGroupContract{
+	vg := &ValidatorGroupContract{}
+	vg.methodMap = make(map[[4]byte]MethodInterface)
+	vg.CreateValidatorGroupMethod()
+	return vg
+}
+func (vg* ValidatorGroupContract)IsPrecompiledContract(address common.Address) bool {
+	return vg.childs.Find(address)
+}
+func (vg* ValidatorGroupContract)CreateValidatorGroupMethod(){
+	bm := &BaseMethod{
+		Name:"createValidatorGroup",
+		Abi:&validatorGroup.ValidatorGroupContractAbi,
+		GasUsed:params.TxGasContractCreation+params.CallValueTransferGas*2,
+	}
+	bm.run = func(input []byte, contract *Contract, evm *EVM) ([]byte, error) {
+		data,err := bm.Inputs().UnpackValues(input[4:])
+		if err!=nil{
+			return nil,err
+		}
+		err = vg.childs.GetState(contract.Address(),evm.StateDB)
+		if err!=nil{
+			return nil,err
+		}
+		nonce := evm.StateDB.GetNonce(evm.Cointyp, contract.CallerAddress)
+		contractAddr := crypto.CreateAddress(contract.CallerAddress, nonce)
+		if vg.IsPrecompiledContract(contractAddr)|| len(evm.StateDB.GetCode(params.MAN_COIN,contractAddr))!=0 {
+			return nil,ErrContractAddressCollision
+		}
+		vg.childs.Insert(contractAddr)
+		err = vg.childs.SetState(contract.Address(),evm.StateDB)
+		if err!=nil{
+			return nil,err
+		}
+		if !evm.StateDB.Exist(evm.Cointyp, contractAddr) {
+			evm.StateDB.CreateAccount(evm.Cointyp, contractAddr)
+		}
+		if !evm.CanTransfer(evm.StateDB, contract.Address(), contract.value, evm.Cointyp){
+			return nil, errors.New("insufficient balance for createValidatorGroup")
+		}
+		evm.Transfer(evm.StateDB, contract.Address(), contractAddr, contract.value, evm.Cointyp)
+		child := NewValidatorGroup()
+		err = child.Constructor(contractAddr,
+			data[0].(common.Address),
+			contract.CallerAddress,
+			data[1].(*big.Int),
+			data[2].(*big.Int),
+			data[3].([]*big.Int),
+			contract,evm)
+		if err!=nil{
+			return nil,err
+		}
+		dType := data[2].(*big.Int)
+		vg.AddCreateValidatorGroupLog(data[0].(common.Address),contractAddr,dType,contract,evm)
+		return contractAddr[:],nil
+	}
+	vg.AddMethod(bm)
+}
+func (vg* ValidatorGroupContract)AddCreateValidatorGroupLog(signAccount,newContract common.Address,dType *big.Int,contract *Contract, evm *EVM)error{
+	topics := []common.Hash{
+		validatorGroup.ValidatorGroupContractAbi.Events["CreateValidatorGroup"].Id(),
+		contract.CallerAddress.Hash(),
+		signAccount.Hash(),
+		newContract.Hash(),
+	}
+	data,err := validatorGroup.ValidatorGroupContractAbi.Events["CreateValidatorGroup"].Inputs.NonIndexed().Pack(contract.value,dType)
+	if err != nil {
+		return err
+	}
+	AddContractLog(topics,data,contract, evm)
+	return nil
+}
+//reward
+func (vg* ValidatorGroupContract)TransferRewards(amount *big.Int,time uint64,contractAddress common.Address,state StateDBManager)error{
+	err := vg.childs.GetState(ValidatorGroupContractAddress,state)
+	if err != nil{
+		return err
+	}
+	if vg.childs.Find(contractAddress){
+		return NewValidatorGroup().TransferRewards(amount,time,contractAddress,state)
+	}
+	return nil
+}
+func (vg* ValidatorGroupContract)TransferInterests(amount *big.Int,position uint64,time uint64,address common.Address,state StateDBManager)error{
+	if position == 0 {
+		return vg.TransferCurrentInterests(amount,time,address,state)
+	}
+	return nil
+}
+//current Interests
+func (vg* ValidatorGroupContract)TransferCurrentInterests(amount *big.Int,time uint64,contractAddress common.Address,state StateDBManager)error{
+	err := vg.childs.GetState(ValidatorGroupContractAddress,state)
+	if err != nil{
+		return err
+	}
+	if vg.childs.Find(contractAddress){
+		return NewValidatorGroup().TransferCurrentInterests(amount,time,contractAddress,state)
+	}
+	return nil
+
+}
+//view
+func (vg* ValidatorGroupContract)ListValidatorGroup(){
+
+}
---
+++ core/vm/validatorGroupState.go
@@ -0,0 +1,325 @@
+// Copyright (c) 2018 The MATRIX Authors
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php
+package vm
+
+import (
+	"github.com/MatrixAINetwork/go-matrix/common"
+	"github.com/MatrixAINetwork/go-matrix/core/vm/validatorGroup"
+	"github.com/MatrixAINetwork/go-matrix/params"
+	"github.com/MatrixAINetwork/go-matrix/rlp"
+	"math"
+	"math/big"
+)
+
+type RateOption struct {
+	Rate    *big.Int
+	Decimal *big.Int
+}
+
+func (ro *RateOption) Mul(a *big.Int) *big.Int {
+	b := new(big.Int).Mul(a, ro.Rate)
+	b.Div(b, ro.Decimal)
+	return b
+}
+
+type DepositInfo struct {
+	Address common.Address
+	Amount  *big.Int
+}
+type RateInfo struct {
+	Threshold *big.Int
+	Rate      RateOption
+}
+
+//Rewards proportion to participants
+type RewardRate struct {
+	OwnerRate RateOption
+	//	CurrentRate RateOption
+	LevelRate []RateInfo
+}
+
+type OwnerInfo struct {
+	Owner           common.Address
+	WithdrawAllTime uint64
+	SignAddress     common.Address `rlp:"-"`
+}
+
+func NewValidatorGroupState() *ValidatorGroupState {
+	return &ValidatorGroupState{
+		depInfo: &MatrixDeposit002{},
+	}
+}
+
+type ValidatorGroupState struct {
+	depInfo      DepositInterface
+	OwnerInfo    OwnerInfo
+	Reward       RewardRate
+	ValidatorMap validatorGroup.ValidatorInfoSlice
+}
+
+func (vc *ValidatorGroupState) SetRewardRate(OwnerRate *big.Int, lvlRate []*big.Int) error {
+	if OwnerRate.Sign() < 0 {
+		return errArguments
+	}
+	vc.Reward.OwnerRate = RateOption{OwnerRate, rateDecmalBig}
+	if len(lvlRate) != len(depLvel) {
+		return errArguments
+	}
+	vc.Reward.LevelRate = make([]RateInfo, len(lvlRate))
+	for i := 0; i < len(lvlRate); i++ {
+		if lvlRate[i].Sign() < 0 {
+			return errArguments
+		}
+		vc.Reward.LevelRate[i].Threshold = depLvel[i]
+		vc.Reward.LevelRate[i].Rate = RateOption{lvlRate[i], rateDecmalBig}
+	}
+	return nil
+}
+
+/*
+func (vc *ValidatorGroupState)CalCurrentWithdrawAmount(amount *big.Int)*big.Int{
+	return vc.Reward.CurrentRate.Mul(amount)
+}
+*/
+func (vc *ValidatorGroupState) SetOwner(contractAddress common.Address, state StateDBManager) error {
+	data, err := rlp.EncodeToBytes(vc.OwnerInfo)
+	if err != nil {
+		return err
+	}
+	state.SetStateByteArray(params.MAN_COIN, contractAddress, EmergeKey(contractAddress, "Owner"), data)
+	return nil
+}
+func (vc *ValidatorGroupState) GetOwner(contractAddress common.Address, state StateDBManager) error {
+	data := state.GetStateByteArray(params.MAN_COIN, contractAddress, EmergeKey(contractAddress, "Owner"))
+	if len(data) == 0 {
+		return nil
+	}
+	err := rlp.DecodeBytes(data, &vc.OwnerInfo)
+	return err
+}
+func (vc *ValidatorGroupState) SetReward(contractAddress common.Address, state StateDBManager) error {
+	data, err := rlp.EncodeToBytes(vc.Reward)
+	if err != nil {
+		return err
+	}
+	state.SetStateByteArray(params.MAN_COIN, contractAddress, EmergeKey(contractAddress, "Reward"), data)
+	return nil
+}
+func (vc *ValidatorGroupState) GetReward(contractAddress common.Address, state StateDBManager) error {
+	data := state.GetStateByteArray(params.MAN_COIN, contractAddress, EmergeKey(contractAddress, "Reward"))
+	if len(data) == 0 {
+		return nil
+	}
+	err := rlp.DecodeBytes(data, &vc.Reward)
+	return err
+}
+func (vc *ValidatorGroupState) SetValidatorMap(contractAddress common.Address, state StateDBManager) error {
+	data, err := rlp.EncodeToBytes(vc.ValidatorMap)
+	if err != nil {
+		return err
+	}
+	state.SetStateByteArray(params.MAN_COIN, contractAddress, EmergeKey(contractAddress, "ValiMap"), data)
+	return nil
+}
+func (vc *ValidatorGroupState) GetAllDepositInfo(contractAddress common.Address, state StateDBManager) *common.DepositBase {
+	newCon := NewContract(AccountRef(contractAddress), AccountRef(developContractAddress), big.NewInt(0), uint64(0), params.MAN_COIN)
+	return vc.depInfo.GetDepositBase(newCon, state, contractAddress)
+
+}
+func (vc *ValidatorGroupState) GetValidatorMap(contractAddress common.Address, time uint64, state StateDBManager) error {
+	data := state.GetStateByteArray(params.MAN_COIN, contractAddress, EmergeKey(contractAddress, "ValiMap"))
+	if len(data) == 0 {
+		return nil
+	}
+	err := rlp.DecodeBytes(data, &vc.ValidatorMap)
+	if err != nil {
+		return err
+	}
+	allDepInfo := vc.GetAllDepositInfo(contractAddress, state)
+	reFundTime := uint64(math.MaxUint64)
+	depMap := make(map[uint64]*common.DepositMsg)
+	if allDepInfo != nil {
+		vc.OwnerInfo.SignAddress = allDepInfo.AddressA1
+		for i := 0; i < len(allDepInfo.Dpstmsg); i++ {
+			msg := &allDepInfo.Dpstmsg[i]
+			depMap[msg.Position] = msg
+		}
+		if len(allDepInfo.Dpstmsg) > 0 && len(allDepInfo.Dpstmsg[0].WithDrawInfolist) > 0 {
+			reFundTime = allDepInfo.Dpstmsg[0].WithDrawInfolist[0].WithDrawTime
+		}
+	}
+	for i := 0; i < len(vc.ValidatorMap); i++ {
+		valiInfo := &vc.ValidatorMap[i]
+		valiInfo.AllAmount = new(big.Int).Set(valiInfo.Current.Amount)
+		for j := 0; j < len(valiInfo.Current.WithdrawList); j++ {
+			if valiInfo.Current.WithdrawList[j].WithDrawTime < reFundTime {
+				valiInfo.Current.WithdrawList = append(valiInfo.Current.WithdrawList[:j], valiInfo.Current.WithdrawList[j+1:]...)
+				j--
+			}
+		}
+		for j := 0; j < len(valiInfo.Positions); j++ {
+			depPos := &valiInfo.Positions[j]
+			if msg, exist := depMap[depPos.Position]; exist {
+				depPos.Amount = msg.DepositAmount
+				if len(msg.WithDrawInfolist) > 0 && msg.WithDrawInfolist[0].WithDrawTime > 0 {
+					depPos.EndTime = msg.EndTime
+				} else {
+					depPos.EndTime = 0
+				}
+				depPos.DType = msg.DepositType
+				if depPos.EndTime == 0 || depPos.EndTime > time {
+					valiInfo.AllAmount.Add(valiInfo.AllAmount, depPos.Amount)
+				}
+			} else {
+				valiInfo.Positions = append(valiInfo.Positions[:j], valiInfo.Positions[j+1:]...)
+				j--
+			}
+
+		}
+	}
+	return err
+}
+func (vc *ValidatorGroupState) SuicideContract(contractAddress common.Address, state StateDBManager) (bool, error) {
+	if vc.CheckEmptyValidatorGroupState(contractAddress, state) {
+		state.Suicide(params.MAN_COIN, contractAddress)
+		parent := &ValidatorContractState{}
+		err := parent.RemoveEmptyValidatorGroup(contractAddress, state)
+		if err == nil {
+			return true, err
+		}
+		return false, err
+	}
+	return false, nil
+}
+func (vc *ValidatorGroupState) CheckValidatorInfo(valiInfo *validatorGroup.ValidatorInfo, contractAddress common.Address, state StateDBManager) error {
+	if valiInfo != nil && len(valiInfo.Positions) == 0 {
+		if valiInfo.Reward.Sign() == 0 && valiInfo.Current.Amount.Sign() == 0 && len(valiInfo.Current.WithdrawList) == 0 && valiInfo.AllAmount.Sign() == 0 {
+			vc.ValidatorMap.Remove(valiInfo.Address)
+
+			//			_,err := vc.SuicideContract(contractAddress,state)
+			//			if err != nil{
+			//				return err
+			//			}
+		}
+	}
+	return nil
+}
+func (vc *ValidatorGroupState) CheckEmptyValidatorGroupState(contractAddress common.Address, state StateDBManager) bool {
+	if len(vc.ValidatorMap) == 0 {
+		balance := state.GetBalance(params.MAN_COIN, contractAddress)
+		if balance[common.MainAccount].Balance.Sign() == 0 {
+			return true
+		}
+	}
+	return false
+}
+func (vc *ValidatorGroupState) SetState(contractAddress common.Address, state StateDBManager) error {
+	suicide, err := vc.SuicideContract(contractAddress, state)
+	if err != nil {
+		return err
+	}
+	if suicide {
+		return nil
+	}
+	err = vc.SetOwner(contractAddress, state)
+	if err != nil {
+		return err
+	}
+	err = vc.SetReward(contractAddress, state)
+	if err != nil {
+		return err
+	}
+	err = vc.SetValidatorMap(contractAddress, state)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (vc *ValidatorGroupState) GetState(contractAddress common.Address, time uint64, state StateDBManager) error {
+	err := vc.GetOwner(contractAddress, state)
+	if err != nil {
+		return err
+	}
+	err = vc.GetReward(contractAddress, state)
+	if err != nil {
+		return err
+	}
+	err = vc.GetValidatorMap(contractAddress, time, state)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (vc *ValidatorGroupState) CalDepositWeight(address common.Address, amount *big.Int) *big.Int {
+	if address == vc.OwnerInfo.Owner {
+		return vc.Reward.OwnerRate.Mul(amount)
+	} else {
+		for i := len(vc.Reward.LevelRate) - 1; i >= 0; i-- {
+			if amount.Cmp(vc.Reward.LevelRate[i].Threshold) >= 0 {
+				return vc.Reward.LevelRate[i].Rate.Mul(amount)
+			}
+		}
+	}
+	return big.NewInt(0)
+}
+func (vc *ValidatorGroupState) DistributeRewards(amount *big.Int) error {
+	weightInfo := make([]DepositInfo, 0, len(vc.ValidatorMap))
+	allWeight := big.NewInt(0)
+	for i := 0; i < len(vc.ValidatorMap); i++ {
+		valiInfo := &vc.ValidatorMap[i]
+		weight := vc.CalDepositWeight(valiInfo.Address, valiInfo.AllAmount)
+		allWeight.Add(allWeight, weight)
+		weightInfo = append(weightInfo, DepositInfo{valiInfo.Address, weight})
+	}
+	leftAmount := new(big.Int).Set(amount)
+	if allWeight.Sign() > 0 {
+		for i, info := range weightInfo {
+			reward := new(big.Int).Mul(amount, info.Amount)
+			reward.Div(reward, allWeight)
+			leftAmount.Sub(leftAmount,reward)
+			vc.ValidatorMap[i].Reward.Add(vc.ValidatorMap[i].Reward, reward)
+		}
+	}
+	if leftAmount.Sign()>0{
+		if index, exist := vc.ValidatorMap.Find(vc.OwnerInfo.Owner);exist {
+			vc.ValidatorMap[index].Reward.Add(vc.ValidatorMap[index].Reward,leftAmount)
+		}
+	}
+	return nil
+
+}
+func (vc *ValidatorGroupState) DistributeCurrentInterests(amount *big.Int) error {
+	if amount.Sign() == 0 {
+		return nil
+	}
+	weightInfo := make([]DepositInfo, 0, len(vc.ValidatorMap))
+	allWeight := big.NewInt(0)
+	for i := 0; i < len(vc.ValidatorMap); i++ {
+		valiInfo := &vc.ValidatorMap[i]
+		amount := valiInfo.Current.Amount
+		if valiInfo.Current.PreAmount.Sign() > 0 && valiInfo.Current.PreAmount.Cmp(valiInfo.Current.Amount) < 0 {
+			amount = valiInfo.Current.PreAmount
+		}
+		weight := vc.CalDepositWeight(valiInfo.Address, amount)
+		allWeight.Add(allWeight, weight)
+		weightInfo = append(weightInfo, DepositInfo{valiInfo.Address, weight})
+		valiInfo.Current.PreAmount.Set(valiInfo.Current.Amount)
+	}
+	leftAmount := new(big.Int).Set(amount)
+	if allWeight.Sign() > 0 {
+		for i, info := range weightInfo {
+			reward := new(big.Int).Mul(amount, info.Amount)
+			reward.Div(reward, allWeight)
+			leftAmount.Sub(leftAmount,reward)
+			vc.ValidatorMap[i].Current.Interest.Add(vc.ValidatorMap[i].Current.Interest, reward)
+		}
+	}
+	if leftAmount.Sign()>0{
+		if index, exist := vc.ValidatorMap.Find(vc.OwnerInfo.Owner);exist {
+			vc.ValidatorMap[index].Current.Interest.Add(vc.ValidatorMap[index].Current.Interest,leftAmount)
+		}
+	}
+	return nil
+
+}
